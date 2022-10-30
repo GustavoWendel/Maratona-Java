20177638fe1be9a1983e98c9723ff353de5a3f93
GustavoWendel <gustavo_wendel@hotmail.com>
2022-10-29 22:25:08 -0300

    Orientação a Objetos - Construtores pt 02 - Sobrecarga

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -5,9 +5,7 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Boku No Hero 2", "tv 2", 24, "ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Boku No Hero 2", "tv", 24, "ação", "Production IG");
         anime.imprime();
-        anime2.imprime();
     }
 }
